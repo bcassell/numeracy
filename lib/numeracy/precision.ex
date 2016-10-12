fe35a942aebe74a7a2aa91d98d949428d1936d4b
bcassell <ben.b.cassell@gmail.com>
2016-10-12 00:37:44 -0700

    adding smallest number

--- lib/numeracy/precision.ex
+++ lib/numeracy/precision.ex
@@ -1,54 +1,64 @@
 defmodule Numeracy.Precision do
 
-  def compute_radix() do
+  # Radix - the base that your computer is in
+  def radix() do
     a = 1.0
-    a = compute_radix_a(a, 0)
+    a = compute_radix_helper(a, 0)
     b = 1.0
     compute_radix(a, b, 0)
   end
 
-  defp compute_radix_a(a, tmp2) when tmp2 - 1.0 == 0.0, do: a
-
-  defp compute_radix_a(a, _) do
+  defp compute_radix_helper(a, tmp2) when tmp2 - 1.0 == 0.0, do: a
+  defp compute_radix_helper(a, _) do
     a = a + a
     tmp1 = a + 1.0
     tmp2 = tmp1 - a
-    compute_radix_a(a, tmp2)
+    compute_radix_helper(a, tmp2)
   end
 
   defp compute_radix(_, _, radix) when radix != 0, do: radix
-
   defp compute_radix(a, b, _) do
     b = b + b
     tmp = a + b
     compute_radix(a, b, round(tmp - a))
   end
 
-  def compute_machine_precision(radix) do
+  # Machine precision - the smallest increment between two successive floating point numbers
+  def machine_precision(radix) do
     inverse_radix = 1.0 / radix
     machine_precision = 1.0
     acc = 1.0 + machine_precision
     compute_machine_precision(machine_precision, inverse_radix, acc)
   end
 
-  def compute_machine_precision(machine_precision, _, acc) when acc - 1.0 == 0.0, do: machine_precision
-
-  def compute_machine_precision(machine_precision, inverse_radix, _) do
+  defp compute_machine_precision(machine_precision, _, acc) when acc - 1.0 == 0.0, do: machine_precision
+  defp compute_machine_precision(machine_precision, inverse_radix, _) do
     machine_precision = machine_precision * inverse_radix
     acc = 1.0 + machine_precision
     compute_machine_precision(machine_precision, inverse_radix, acc)
   end
 
-  def compute_negative_machine_precision(radix) do
+  # Negative machine precision - the smallest decrement between two successive floating point numbers
+  def negative_machine_precision(radix) do
     inverse_radix = 1.0 / radix
     machine_precision = 1.0
     acc = 1.0 - machine_precision
     compute_negative_machine_precision(machine_precision, inverse_radix, acc)
   end
 
-  def compute_negative_machine_precision(machine_precision, inverse_radix, _) do
+  defp compute_negative_machine_precision(machine_precision, inverse_radix, _) do
     machine_precision = machine_precision * inverse_radix
     acc = 1.0 - machine_precision
     compute_machine_precision(machine_precision, inverse_radix, acc)
   end
+
+  # Smallest number - the smallest representable positive number
+  def smallest_number(radix, negative_machine_precision) do
+    inverse_radix = 1.0/radix
+    full_mantissa_number = 1.0 - radix * negative_machine_precision
+    compute_smallest_number(full_mantissa_number, inverse_radix)
+  end
+
+  defp compute_smallest_number(full_mantissa_number, inverse_radix) when full_mantissa_number * inverse_radix == 0.0, do: full_mantissa_number
+  defp compute_smallest_number(full_mantissa_number, inverse_radix), do: compute_smallest_number(full_mantissa_number * inverse_radix, inverse_radix)
 end
--- test/precision_test.exs
+++ test/precision_test.exs
@@ -4,16 +4,22 @@ defmodule PrecisionTest do
   import Numeracy.Precision
 
   test "can compute radix" do
-    assert compute_radix() == 2
+    assert radix() == 2
   end
 
   test "can compute machine precision" do
     radix = 2
-    assert compute_machine_precision(radix) == 1.1102230246251565e-16
+    assert machine_precision(radix) == 1.1102230246251565e-16
   end
 
   test "can compute negative machine precision" do
     radix = 2
-    assert compute_negative_machine_precision(radix) == 1.1102230246251565e-16
+    assert negative_machine_precision(radix) == 1.1102230246251565e-16
+  end
+
+  test "can compute smallest number" do
+    radix = 2
+    negative_machine_precision = 1.1102230246251565e-16
+    assert smallest_number(radix, negative_machine_precision) == 5.0e-324
   end
 end
